ab5fe6846c409c858876cecc9399a1dccbde1b8e
JohannesBoeh <johannes.boehler@cbrehotels.com>
2026-08-19 16:13:15 +0200

    Add city configurations

--- scripts/test.py
+++ scripts/test.py
@@ -1,1 +1,13 @@
-print("Python funktioniert!")
+cities = [
+    "Berlin",
+    "Düsseldorf",
+    "Frankfurt",
+    "Hamburg",
+    "Köln",
+    "Leipzig",
+    "München",
+    "Stuttgart"
+]
+
+for city in cities:
+    print(city)
