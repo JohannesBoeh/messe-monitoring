f0a2a5e709f6c3c26ea0f0b326776678592ca07f
JohannesBoeh <johannes.boehler@cbrehotels.com>
2026-08-19 16:25:26 +0200

    Add browser headers

--- scripts/auma_scraper.py
+++ scripts/auma_scraper.py
@@ -1,11 +1,16 @@
-from cities import CITIES
 import requests
 
 print("Teste Verbindung zu AUMA...")
 
 url = "https://www.auma.de"
 
-response = requests.get(url)
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+response = requests.get(url, headers=headers)
 
 print(f"Status Code: {response.status_code}")
 print(f"Inhalt erhalten: {len(response.text)} Zeichen")
+
+print(response.text[:500])
