c0b2b787dc29e2107268cfea8d057b1cb24791c1
JohannesBoeh <johannes.boehler@cbrehotels.com>
2026-08-19 16:14:29 +0200

    Add city configurations

---
+++ scripts/cities.py
@@ -0,0 +1,10 @@
+CITIES = [
+    {"city": "Berlin", "class": "A"},
+    {"city": "Düsseldorf", "class": "A"},
+    {"city": "Frankfurt", "class": "A"},
+    {"city": "Hamburg", "class": "A"},
+    {"city": "Köln", "class": "A"},
+    {"city": "Leipzig", "class": "A"},
+    {"city": "München", "class": "A"},
+    {"city": "Stuttgart", "class": "A"}
+]
